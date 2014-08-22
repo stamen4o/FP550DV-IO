d6a451cf09d8196552987e6a3548b21720d6e0e2
stamen4o <stamen4o@yahoo.com>
2014-08-22 15:50:20 +0300

    version 1.0

---
+++ service550dv/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace service550dv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.отчетиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дневенXОтчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дневенZОтчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.диагностикаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 50);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "ВЪВЕДИ СУМА";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 38);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 40);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "СВЪРЗВАНЕ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LawnGreen;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(67, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 38);
+            this.label3.TabIndex = 15;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(153, 179);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 50);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "ИЗВЕДИ СУМА";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отчетиToolStripMenuItem,
+            this.диагностикаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(252, 24);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // отчетиToolStripMenuItem
+            // 
+            this.отчетиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дневенXОтчетToolStripMenuItem,
+            this.дневенZОтчетToolStripMenuItem});
+            this.отчетиToolStripMenuItem.Name = "отчетиToolStripMenuItem";
+            this.отчетиToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.отчетиToolStripMenuItem.Text = "Отчети";
+            // 
+            // дневенXОтчетToolStripMenuItem
+            // 
+            this.дневенXОтчетToolStripMenuItem.Name = "дневенXОтчетToolStripMenuItem";
+            this.дневенXОтчетToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.дневенXОтчетToolStripMenuItem.Text = "Дневен X отчет";
+            this.дневенXОтчетToolStripMenuItem.Click += new System.EventHandler(this.дневенXОтчетToolStripMenuItem_Click);
+            // 
+            // дневенZОтчетToolStripMenuItem
+            // 
+            this.дневенZОтчетToolStripMenuItem.Name = "дневенZОтчетToolStripMenuItem";
+            this.дневенZОтчетToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.дневенZОтчетToolStripMenuItem.Text = "Дневен Z отчет";
+            this.дневенZОтчетToolStripMenuItem.Click += new System.EventHandler(this.дневенZОтчетToolStripMenuItem_Click);
+            // 
+            // диагностикаToolStripMenuItem
+            // 
+            this.диагностикаToolStripMenuItem.Name = "диагностикаToolStripMenuItem";
+            this.диагностикаToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.диагностикаToolStripMenuItem.Text = "Диагностика";
+            this.диагностикаToolStripMenuItem.Click += new System.EventHandler(this.диагностикаToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(14, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(14, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(47, 47);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(252, 242);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(268, 281);
+            this.MinimumSize = new System.Drawing.Size(268, 281);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FP550DV-IO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem отчетиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дневенXОтчетToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дневенZОтчетToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem диагностикаToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ service550dv/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using FP3530;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace service550dv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            textBox2.TextAlign = HorizontalAlignment.Right;
+        }
+        public int comPortSelected = 1;
+        public int speedPortSelected = 9600;
+         private void button3_Click(object sender, EventArgs e)
+        {
+            string exitCode = "";
+            string cashSum = "";
+            string servIn = "";
+            string servOut = "";
+            try
+            {
+                FP3530.CS_BGR_FP550_D_KL fp = new CS_BGR_FP550_D_KLClass();
+                fp.INIT_Ex1(comPortSelected,speedPortSelected);
+                fp.CMD_70_0_0(textBox2.Text.ToString(),ref exitCode,ref cashSum,ref servIn,ref servOut);
+                if (exitCode == "P")
+                {
+                    double cash = Convert.ToDouble(cashSum);
+                    double servI = Convert.ToDouble(servIn);
+                    double servO = Convert.ToDouble(servOut);
+                    MessageBox.Show("Операцията е успешна\n" + "Касова наличност : " + cash / 100 + " лв.\n" + "Общо въведени за деня: " + servI / 100 + " лв.\n" + "Общо изведени за деня: " + servO / 100 + " лв.\n");
+                }
+                else
+                {
+                    double cash = Convert.ToDouble(cashSum);
+                    double servI = Convert.ToDouble(servIn);
+                    double servO = Convert.ToDouble(servOut);
+                    MessageBox.Show("Операцията е неуспешна\n" + "Касова наличност : " + cash / 100 + " лв.\n" + "Общо въведени за деня: " + servI / 100 + " лв.\n" + "Общо изведени за деня: " + servO / 100 + " лв.\n");
+                }
+                
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Проблем с въвеждането");
+            }    
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FP3530.CS_BGR_FP550_D_KL fp = new CS_BGR_FP550_D_KLClass();
+            try
+            {
+                string comPort = System.IO.File.ReadAllText(@"C:\FP550DV-IO\FP550DV-IO\comNumber.txt");
+                comPortSelected = Convert.ToInt32(comPort);
+            }
+            catch
+            {
+                MessageBox.Show(@"Проблем със номера за COM порт.");
+            }
+            try 
+	        {
+                string comSpeed = System.IO.File.ReadAllText(@"C:\FP550DV-IO\FP550DV-IO\comSpeed.txt");
+                speedPortSelected = Convert.ToInt32(comSpeed);
+	        }
+	        catch (Exception)
+	        {
+		        MessageBox.Show(@"Проблем със скороста за COM порт.");
+	        }
+            string printerName = "";
+            string fwRev = "";
+            string fwDate = "";
+            string fwTime = "";
+            string chK = "";
+            string sW = "";
+            string serial = "";
+            string fm = "";
+            try
+            {
+                fp.INIT_Ex1(comPortSelected,speedPortSelected);
+                fp.CMD_90_0_0(ref printerName, ref fwRev, ref fwDate, ref fwTime, ref chK, ref sW, ref serial, ref fm);
+                label3.Text = serial;
+                if (serial != null)
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = false;
+                }
+                else
+                {
+                    pictureBox2.Visible = true;
+                    pictureBox1.Visible = false;
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Проверете данни за COM порт.");
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string exitCode = "";
+            string cashSum = "";
+            string servIn = "";
+            string servOut = "";
+            try
+            {
+                FP3530.CS_BGR_FP550_D_KL fp = new CS_BGR_FP550_D_KLClass();
+                fp.INIT_Ex1(comPortSelected, speedPortSelected);
+                fp.CMD_70_0_0("-" + textBox2.Text.ToString(), ref exitCode, ref cashSum, ref servIn, ref servOut);
+                if (exitCode == "P")
+                {
+                    double cash = Convert.ToDouble(cashSum);
+                    double servI = Convert.ToDouble(servIn);
+                    double servO = Convert.ToDouble(servOut);
+                    MessageBox.Show("Операцията е успешна\n" + "Касова наличност : " + cash / 100 + " лв.\n" + "Общо въведени за деня: " + servI / 100 + " лв.\n" + "Общо изведени за деня: " + servO / 100 + " лв.\n");
+                }
+                else
+                {
+                    double cash = Convert.ToDouble(cashSum);
+                    double servI = Convert.ToDouble(servIn);
+                    double servO = Convert.ToDouble(servOut);
+                    MessageBox.Show("Операцията е неуспешна\n" + "Касова наличност : " + cash / 100 + " лв.\n" + "Общо въведени за деня: " + servI / 100 + " лв.\n" + "Общо изведени за деня: " + servO / 100 + " лв.\n");
+                }
+                
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Проблем с извеждането");
+            }    
+        }
+
+        private void дневенXОтчетToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string closure = "";
+            string fmTotal = "";
+            string totA = "";
+            string totB = "";
+            string totC = "";
+            string totD = "";
+            string totE = "";
+            string totF = "";
+            string totG = "";
+            string totH = "";
+            FP3530.CS_BGR_FP550_D_KL fp = new CS_BGR_FP550_D_KLClass();
+            fp.INIT_Ex1(comPortSelected, speedPortSelected);
+            fp.CMD_69_0_0("2", "N", ref closure, ref fmTotal, ref totA, ref totB, ref totC, ref totD, ref totE, ref totF, ref totG, ref totH);
+        }
+
+        private void дневенZОтчетToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string closure = "";
+            string fmTotal = "";
+            string totA = "";
+            string totB = "";
+            string totC = "";
+            string totD = "";
+            string totE = "";
+            string totF = "";
+            string totG = "";
+            string totH = "";
+            FP3530.CS_BGR_FP550_D_KL fp = new CS_BGR_FP550_D_KLClass();
+            fp.INIT_Ex1(comPortSelected, speedPortSelected);
+            fp.CMD_69_0_0("0", "", ref closure, ref fmTotal, ref totA, ref totB, ref totC, ref totD, ref totE, ref totF, ref totG, ref totH);
+        }
+
+        private void диагностикаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             FP3530.CS_BGR_FP550_D_KL fp = new CS_BGR_FP550_D_KLClass();
+            try
+            {
+                string comPort = System.IO.File.ReadAllText(@"C:\FP550DV-IO\FP550DV-IO\comNumber.txt");
+                comPortSelected = Convert.ToInt32(comPort);
+            }
+            catch
+            {
+                MessageBox.Show(@"Проблем със номера за COM порт.");
+            }
+            try 
+	        {
+                string comSpeed = System.IO.File.ReadAllText(@"C:\FP550DV-IO\FP550DV-IO\comSpeed.txt");
+                speedPortSelected = Convert.ToInt32(comSpeed);
+	        }
+	        catch (Exception)
+	        {
+                MessageBox.Show(@"Проблем със скороста за COM порт.");
+	        }
+            try
+            {
+                fp.INIT_Ex1(comPortSelected,speedPortSelected);
+                fp.CMD_71_0_0();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Проблем с комуникацията");
+            }            
+        }
+    }         
+}
+    
+
+
